0b34f23a4ce655dfcb744e14aa340ffc07929ede
Charles Morman <github@cmorman.com>
2024-10-24 16:16:41 -0700

    feat: starting develop branch, Task class

---
+++ app/task.py
@@ -0,0 +1,18 @@
+# To Do Class:
+
+
+
+
+class Task:
+    '''
+    `Task` classes holds the 
+    '''
+    def __init__(self, id=None, title="Untitled Task", description=None,
+                due_date=None, priority=None):
+        self.id = id                    # placeholder
+        self.title = title
+        self.description = description
+        self.due_date = due_date
+        self.priority = priority
+
+        
