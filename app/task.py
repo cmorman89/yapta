341ba4432daa6a42ecdf9d966abe27e0d9463506
Charles Morman <github@cmorman.com>
2024-10-24 16:30:20 -0700

    feat: `Tasks` class to hold task-related data

--- app/task.py
+++ app/task.py
@@ -1,18 +1,23 @@
-# To Do Class:
+'''
+Module:     task.py
+Descrp:     Defines the `Task` class that holds task data
+Author:     Charles Morman
+
+'''
 
 
+from dataclasses import dataclass
 
 
+@dataclass
 class Task:
     '''
-    `Task` classes holds the 
+    Responsibility: `Task` class primarily holds task-related data and identifiers
     '''
     def __init__(self, id=None, title="Untitled Task", description=None,
-                due_date=None, priority=None):
+                 due_date=None, priority=None):
         self.id = id                    # placeholder
         self.title = title
         self.description = description
         self.due_date = due_date
         self.priority = priority
-
-        
