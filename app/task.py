c6123c4251ff77354f908de948ef25c6d15b3b95
Charles Morman <github@cmorman.com>
2024-10-24 17:38:08 -0700

    feat: toggle task compelte/not complete function - green phase

--- app/task.py
+++ app/task.py
@@ -13,37 +13,38 @@ from dataclasses import dataclass
 class Task:
     '''
     Responsibility: `Task` class primarily holds task-related data and
-        other identifiers
+        other identifiers, updates self values
     '''
-    def __init__(self, id=None, title="Untitled Task", description=None,
+    def __init__(self, task_id=None, title="Untitled Task", description=None,
                  due_date=None, priority=None, complete=False):
-        self.id = id                    # placeholder
+        self.task_id = task_id                    # placeholder
         self.title = title
         self.description = description
         self.due_date = due_date
         self.priority = priority
         self.complete = complete
 
-  
     def update_title(self, title):
+        '''Update task `title`'''
         self.title = title
         return self
 
-
     def update_description(self, description):
+        '''Update task `description`'''
         self.description = description
         return self
 
-
     def update_due_date(self, due_date):
+        '''Update task `due_date`'''
         self.due_date = due_date
         return self
 
-
     def update_priority(self, priority):
+        '''Update task `priority`'''
         self.priority = priority
         return self
 
     def toggle_complete(self):
+        '''Toggle task `complete` boolean'''
         self.complete = not self.complete
         return self
--- tests/test_task.py
+++ tests/test_task.py
@@ -12,15 +12,41 @@ TASK_DESC = "Remember to get milk from the store."
 TASK_DUE_DATE = datetime(2024, 9, 30)
 TASK_DUE_DATE_STR = "9/30/2024"
 TASK_PRIORITY = "High"
+# Template_Task = Task(task_id=TASK_ID, title=TASK_TITLE, description=TASK_DESC,
+#                    due_date=TASK_DUE_DATE, priority=TASK_PRIORITY, complete=True)
 
 
 def test_task_construction():
     '''Tests the `Tasks` constructor and attributes'''
-    task = Task(id=TASK_ID, title=TASK_TITLE, description=TASK_DESC,
+    task = Task(task_id=TASK_ID, title=TASK_TITLE, description=TASK_DESC,
                 due_date=TASK_DUE_DATE, priority=TASK_PRIORITY, complete=True)
-    assert task.id == TASK_ID
+    assert task.task_id == TASK_ID
     assert task.title == TASK_TITLE
     assert task.description == TASK_DESC
     assert task.due_date == TASK_DUE_DATE
     assert task.priority == TASK_PRIORITY
     assert task.complete is True
+
+
+def test_task_setters():
+    '''Tests the various setters in `Task`'''
+    task = Task(task_id=TASK_ID)
+    (task.update_title(TASK_TITLE).update_description(TASK_DESC)
+        .update_due_date(TASK_DUE_DATE).update_priority(TASK_PRIORITY)
+        .toggle_complete())
+    assert task.task_id == TASK_ID
+    assert task.title == TASK_TITLE
+    assert task.description == TASK_DESC
+    assert task.due_date == TASK_DUE_DATE
+    assert task.priority == TASK_PRIORITY
+    assert task.complete is True
+
+
+def test_task_complete_toggle():
+    '''Toggle boolean back and forth'''
+    task = Task()
+    assert not task.complete
+    task.toggle_complete()
+    assert task.complete
+    task.toggle_complete()
+    assert not task.complete
