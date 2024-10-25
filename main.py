a60ff31d2fd672bc6fc2769145e953b125230ddd
Charles Morman <github@cmorman.com>
2024-10-25 16:24:15 -0700

    feat: main.py prints a list

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-''''''
+
+
+from app.task import Task
+from app.task_list import TaskList
+
+
+if __name__ == "__main__":
+    task_1 = Task(task_name="Item 1")
+    task_2 = Task(task_name="Item 2")
+    task_3 = Task(task_name="Item 3")
+
+    task_list = TaskList()
+
+    task_list.add_task(task_1).add_task(task_2).add_task(task_3)
+
+    task_list.print_todo()
