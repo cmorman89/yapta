a4d51b2eec8266580b87a2215000d55da3da17bd
Charles Morman <github@cmorman.com>
2024-10-25 08:57:23 -0700

    fix: restore missing code

--- app/task.py
+++ app/task.py
@@ -23,3 +23,28 @@ class Task:
         self.due_date = due_date
         self.priority = priority
         self.complete = complete
+
+    def update_title(self, title):
+        '''Update task `title`'''
+        self.title = title
+        return self
+
+    def update_description(self, description):
+        '''Update task `description`'''
+        self.description = description
+        return self
+
+    def update_due_date(self, due_date):
+        '''Update task `due_date`'''
+        self.due_date = due_date
+        return self
+
+    def update_priority(self, priority):
+        '''Update task `priority`'''
+        self.priority = priority
+        return self
+
+    def toggle_complete(self):
+        '''Toggle task `complete` boolean'''
+        self.complete = not self.complete
+        return self
