7c3b543a682d226f3ab85e3af5b1357bf1e16c19
Charles Morman <github@cmorman.com>
2024-10-25 22:50:51 -0700

    feat: print formatted `Task` objects

---
+++ app/utils/task_printer.py
@@ -0,0 +1,51 @@
+"""
+Module:         task_printer.py
+Description:    Prints a formatted Task card to console
+Author:         Charles Morman
+"""
+
+
+from dataclasses import dataclass
+
+
+@dataclass
+class TaskPrinter:
+    """
+    A class used to print the details of a task in a formatted manner.
+
+    Attributes
+    ----------
+    task : Task
+        An instance of the `Task` class containing task details.
+
+    Methods
+    -------
+    __init__(task)
+        Initializes the `TaskPrinter` with a task and prints its details.
+    """
+    @staticmethod
+    def task_card_string(task):
+        """
+        Return a formatted card string
+        """
+        output = (
+            f"""
+            ====================================================
+            Task Name: {task.task_name}
+            ----------------------------------------------------
+            - Priority:    {task.priority}
+            - Description: {task.description}
+            - Due Date:    {task.due_date}
+            - Status:      {'Completed' if task.complete else "Incomplete"}
+            ====================================================
+            """
+        ).strip(" ")
+
+        return output
+
+    @staticmethod
+    def print_task(task):
+        """
+        Get and the print the formatted card string
+        """
+        print(TaskPrinter.task_card_string(task))
---
+++ tests/test_utils/test_task_printer.py
@@ -0,0 +1,46 @@
+"""Tests the app/utils/task_printer.py module"""
+
+
+from datetime import datetime
+from app.task import Task
+from app.utils.task_printer import TaskPrinter
+
+
+# Global/common inputs
+TASK_TITLE = "Get Milk"
+TASK_DESC = "Remember to get milk from the store."
+TASK_DUE_DATE = datetime(2024, 9, 30)
+TASK_DUE_DATE_STR = "9/30/2024"
+TASK_PRIORITY = "High"
+Template_Task = Task(
+    task_name=TASK_TITLE,
+    description=TASK_DESC,
+    due_date=TASK_DUE_DATE,
+    priority=TASK_PRIORITY,
+    complete=True,
+)
+
+
+def test_task_printer():
+    '''Test the formatted output of TaskPrinter'''
+    task = Task(
+        task_name=TASK_TITLE,
+        description=TASK_DESC,
+        due_date=TASK_DUE_DATE,
+        priority=TASK_PRIORITY,
+        complete=True,
+        )
+    output = (
+        f"""
+            ====================================================
+            Task Name: {task.task_name}
+            ----------------------------------------------------
+            - Priority:    {task.priority}
+            - Description: {task.description}
+            - Due Date:    {task.due_date}
+            - Status:      {'Completed' if task.complete else "Incomplete"}
+            ====================================================
+        """
+    ).strip(" ")
+
+    assert TaskPrinter.task_card_string(task) == output
